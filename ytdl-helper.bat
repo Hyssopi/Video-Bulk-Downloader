c877346c8cf0857aa4f81dbf882993a7b845e3cb
Hyssopi <43843070+Hyssopi@users.noreply.github.com>
2026-06-22 01:46:59 -0700

    Update

--- ytdl-helper.bat
+++ ytdl-helper.bat
@@ -1,3 +1,3 @@
-yt-dlp_win\yt-dlp.exe --embed-thumbnail --embed-chapters -o "Downloaded/%%(id)s.%%(ext)s" --batch-file "ytdl-helper-links.txt"
+yt-dlp_win\yt-dlp.exe --ffmpeg-location "ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe" --embed-thumbnail --embed-chapters -o "Downloaded/%%(id)s.%%(ext)s" --batch-file "ytdl-helper-links.txt"
 
 PAUSE
